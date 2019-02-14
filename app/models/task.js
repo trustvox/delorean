03c8fe569dad8e07ded2b936ae2870946948aa98
Bruno Casali <brunoocasali@gmail.com>
2019-02-14 19:29:59 -0200

    Comment out some properties and add initial/ending to be saved

--- app/models/task.js
+++ app/models/task.js
@@ -8,6 +8,8 @@ const {
 
 export default Model.extend({
   description: attr('string'),
+  initial: attr('string'),
+  ending: attr('string'),
 
   // trackings: hasMany('tracking', { async: true, dependent: 'destroy' })
 });
