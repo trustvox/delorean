796f583f0382e2b52edbc0944627e2cba95bcc21
Bruno Casali <brunoocasali@gmail.com>
2020-08-07 14:10:22 -0300

    Improve navbar look n feel + Add a modifier to render icons

--- ember-cli-build.js
+++ ember-cli-build.js
@@ -26,7 +26,9 @@ module.exports = function (defaults) {
   // please specify an object with the list of modules as keys
   // along with the exports of each module as its value.
 
-  app.import(`node_modules/feather-icons/dist/feather.min.js`);
+  app.import("node_modules/feather-icons/dist/feather.min.js", {
+    using: [{ transformation: "cjs", as: "feather" }],
+  });
 
   return app.toTree();
 };
